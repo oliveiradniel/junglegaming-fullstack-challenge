8a045f1310b6d8bbf34dda4097035cbb0ec0be55
oliveiradniel <kadadniel@gmail.com>
2025-11-27 14:29:37 -0300

    refactor(shared): rename userId to authorId in audit log types

--- packages/shared/types/task-audit-log/list.ts
+++ packages/shared/types/task-audit-log/list.ts
@@ -3,28 +3,34 @@ import { UserWithoutPassword } from "entities";
 export interface ListCreationTaskAuditLog {
   id: string;
   taskId: string;
-  userId: string;
+  authorId: string;
   taskTitle: string;
   newValue: string;
   changedAt: Date;
 }
 
+export type ListCreationTaskAuditLogWithAuthorData = ListCreationTaskAuditLog & { authorData: UserWithoutPassword }
+
 export interface ListDeletionTaskAuditLog {
   id: string;
   taskId: string;
-  userId: string;
+  authorId: string;
   taskTitle: string;
   oldValue: string;
   changedAt: Date;
 }
 
+export type ListDeletionTaskAuditLogWithAuthorData = ListDeletionTaskAuditLog & { authorData: UserWithoutPassword }
+
 export interface ListUpdateTaskAuditLog {
   id: string;
   taskId: string;
-  userId: string;
+  authorId: string;
   taskTitle: string;
   fieldName: string;
   oldValue: string | UserWithoutPassword[];
   newValue: string | UserWithoutPassword[];
   changedAt: Date;
 }
+
+export type ListUpdateTaskAuditLogWithAuthorData = ListUpdateTaskAuditLog & { authorData: UserWithoutPassword }
