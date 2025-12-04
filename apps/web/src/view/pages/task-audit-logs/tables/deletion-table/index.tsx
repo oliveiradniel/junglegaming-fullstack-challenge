d75a3a989a957bbb93394e9776f5d67a8c9538ef
oliveiradniel <kadadniel@gmail.com>
2025-12-04 18:27:25 -0300

    refactor(web): extract empty state component for task deletion audit log

--- apps/web/src/view/pages/task-audit-logs/tables/deletion-table/index.tsx
+++ apps/web/src/view/pages/task-audit-logs/tables/deletion-table/index.tsx
@@ -1,16 +1,11 @@
-import { Link } from '@tanstack/react-router';
-
 import { useListTaskDeletionAuditLogQuery } from '@/app/hooks/queries/use-list-task-deletion-audit-log-query';
 import { useColumns } from './use-columns';
 
-import { BookOpenText } from 'lucide-react';
-
 import { DataTable } from '@/view/components/data-table';
-import { EmptyData } from '@/view/components/empty-data';
-import { Button } from '@/view/components/ui/button';
 import { DataTableColumnsVisibilityDropdown } from '@/view/components/data-table/data-table-columns-visibility-dropdown';
 import { DataTableContent } from '@/view/components/data-table/data-table-content';
 import { DataTableFallback } from '@/view/components/data-table/data-table-fallback';
+import { EmptyLog } from './empty-log';
 
 export function TaskDeletionAuditLogTable() {
   const { taskDeletionAuditLogsList, isTaskDeletionAuditLogsLoading } =
@@ -21,21 +16,7 @@ export function TaskDeletionAuditLogTable() {
   return (
     <>
       {!isTaskDeletionAuditLogsLoading &&
-        taskDeletionAuditLogsList.length === 0 && (
-          <EmptyData>
-            <p className="max-w-[500px] text-center">
-              Não há registro de nenhuma tarefa excluída para ser listada na
-              auditoria de exclusão. Você pode excluir qualquer tarefa a partir
-              da listagem principal.
-            </p>
-
-            <Button onClick={() => {}} className="p-6">
-              <Link to="/tasks" className="flex items-center gap-2">
-                <BookOpenText className="size-4!" /> Ir até as tarefas
-              </Link>
-            </Button>
-          </EmptyData>
-        )}
+        taskDeletionAuditLogsList.length === 0 && <EmptyLog />}
 
       {isTaskDeletionAuditLogsLoading && (
         <DataTableFallback
