dd20759cc810aa1403d9c1e4a49f3ceb8a202fb0
oliveiradniel <kadadniel@gmail.com>
2025-12-04 18:27:45 -0300

    refactor(web): extract empty state component for task creation audit log

--- apps/web/src/view/pages/task-audit-logs/tables/creation-table/index.tsx
+++ apps/web/src/view/pages/task-audit-logs/tables/creation-table/index.tsx
@@ -1,44 +1,22 @@
-import { useTasks } from '@/app/hooks/use-tasks';
 import { useListTaskCreationAuditLogQuery } from '@/app/hooks/queries/use-list-task-creation-audit-log-query';
 import { useColumns } from './use-columns';
 
-import { Plus } from 'lucide-react';
-import { cn } from '@/lib/utils';
-
-import { Button } from '@/view/components/ui/button';
 import { DataTable } from '@/view/components/data-table';
-import { EmptyData } from '@/view/components/empty-data';
 import { DataTableColumnsVisibilityDropdown } from '@/view/components/data-table/data-table-columns-visibility-dropdown';
 import { DataTableContent } from '@/view/components/data-table/data-table-content';
 import { DataTableFallback } from '@/view/components/data-table/data-table-fallback';
+import { EmptyLog } from './empty-log';
 
 export function TaskCreationAuditLogTable() {
-  const { handleOpenNewTaskSheet } = useTasks();
-
   const { taskCreationAuditLogsList, isTaskCreationAuditLogsLoading } =
     useListTaskCreationAuditLogQuery();
 
   const columns = useColumns();
 
-  const textStyles = 'text-primary font-medium';
-
   return (
     <div className="h-full">
       {!isTaskCreationAuditLogsLoading &&
-        taskCreationAuditLogsList.length === 0 && (
-          <EmptyData>
-            <p className="max-w-[500px] text-center">
-              Não há registro de nenhuma tarefa criada para ser listada na
-              auditoria de criação. Que tal adicionar uma agora e melhorar o seu
-              <span className={cn(textStyles)}> fluxo de trabalho</span> e o seu
-              <span className={cn(textStyles)}> controle de prazos</span>?
-            </p>
-
-            <Button onClick={handleOpenNewTaskSheet} className="p-6!">
-              <Plus className="size-4!" /> Adicionar tarefa
-            </Button>
-          </EmptyData>
-        )}
+        taskCreationAuditLogsList.length === 0 && <EmptyLog />}
 
       {isTaskCreationAuditLogsLoading && (
         <DataTableFallback
